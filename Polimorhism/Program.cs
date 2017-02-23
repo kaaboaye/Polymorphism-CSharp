47c06eb907fb4cc0203bf1eeb96d875c57066ccf
Mieszko Wawrzyniak <kaaboaye@gmail.com>
2017-02-23 23:09:06 +0100

    1.0.3

--- Polimorhism/Program.cs
+++ Polimorhism/Program.cs
@@ -1,9 +1,6 @@
-﻿using System;
+﻿using Polimorhism.Shapes;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Polimorhism.Shapes;
 
 namespace Polimorhism
 {
--- Polimorhism/ShapeTypes.cs
+++ Polimorhism/ShapeTypes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Polimorhism.Shapes
+﻿namespace Polimorhism.Shapes
 {
     public enum ShapeType
     {
